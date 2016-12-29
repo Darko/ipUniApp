9470aba0d4cdd3d655b36034e6997c481b40d1c7
Darko <espadomkd@gmail.com>
2016-12-29 13:41:53 +0100

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var mamp = require('gulp-mamp');
+
+var options = {};
+ 
+gulp.task('start', function(cb){
+  mamp(options, 'start', cb);
+});
+ 
+gulp.task('stop', function(cb){
+  mamp(options, 'stop', cb);
+});
+ 
+gulp.task('mamp', ['start']);
+
+gulp.task('sass', function () {
+  gulp.src('sass/**/*')
+  // .pipe(sass({includePaths: ['sass'], outputStyle: 'compressed'}))
+  .pipe(sass())
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+  gulp.watch("sass/**/*.sass", ['sass']);
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init(["src/css/*.css", "src/scripts/*.js", '*.html', "views/**/*.html"], {
+    proxy: 'localhost:8888/ipUniApp/'
+  });
+});
+
+gulp.task('default', ['mamp','browser-sync', 'watch']);
+
