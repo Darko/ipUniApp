d5fb0cef87b39efb30d7f3571f8321f910e89ecc
Darko <espadomkd@gmail.com>
2017-02-08 17:59:31 +0100

    database structure

--- ip_app.sql
+++ ip_app.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Feb 08, 2017 at 05:07 PM
+-- Generation Time: Feb 08, 2017 at 05:59 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -17,6 +17,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `playlists`
+--
+
+CREATE TABLE `playlists` (
+  `id` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `createdAt` date NOT NULL,
+  `private` tinyint(1) NOT NULL,
+  `songsCount` int(11) NOT NULL,
+  `likes` int(11) NOT NULL,
+  `dislikes` int(11) NOT NULL,
+  `thumbnail` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlist_contents`
+--
+
+CREATE TABLE `playlist_contents` (
+  `id` int(11) NOT NULL,
+  `playlistId` int(11) NOT NULL,
+  `songId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `playlist_followers`
 --
 
@@ -24,18 +53,79 @@ CREATE TABLE `playlist_followers` (
   `id` int(11) NOT NULL,
   `playlistId` int(11) NOT NULL,
   `userId` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `playlist_followers`
+-- Table structure for table `playlist_identity`
 --
 
-INSERT INTO `playlist_followers` (`id`, `playlistId`, `userId`) VALUES
-(1, 1, 1);
+CREATE TABLE `playlist_identity` (
+  `id` int(11) NOT NULL,
+  `playlistId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `songs`
+--
+
+CREATE TABLE `songs` (
+  `id` int(11) NOT NULL,
+  `title` varchar(130) NOT NULL,
+  `channelTitle` varchar(50) NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `videoId` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `expiresIn` int(11) NOT NULL,
+  `tokenType` varchar(30) NOT NULL,
+  `accessToken` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `role` varchar(30) NOT NULL,
+  `avatar` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `playlists`
+--
+ALTER TABLE `playlists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `playlist_contents`
+--
+ALTER TABLE `playlist_contents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `playlistId` (`playlistId`),
+  ADD KEY `songId` (`songId`);
 
 --
 -- Indexes for table `playlist_followers`
@@ -46,17 +136,81 @@ ALTER TABLE `playlist_followers`
   ADD KEY `userId` (`userId`);
 
 --
+-- Indexes for table `playlist_identity`
+--
+ALTER TABLE `playlist_identity`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `playlistId` (`playlistId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tokens`
+--
+ALTER TABLE `tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `playlists`
+--
+ALTER TABLE `playlists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `playlist_contents`
+--
+ALTER TABLE `playlist_contents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `playlist_followers`
 --
 ALTER TABLE `playlist_followers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `playlist_identity`
+--
+ALTER TABLE `playlist_identity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tokens`
+--
+ALTER TABLE `tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `playlist_contents`
+--
+ALTER TABLE `playlist_contents`
+  ADD CONSTRAINT `playlist_contents_ibfk_1` FOREIGN KEY (`playlistId`) REFERENCES `playlists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `playlist_contents_ibfk_2` FOREIGN KEY (`songId`) REFERENCES `songs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `playlist_followers`
@@ -64,3 +218,10 @@ ALTER TABLE `playlist_followers`
 ALTER TABLE `playlist_followers`
   ADD CONSTRAINT `playlist_followers_ibfk_1` FOREIGN KEY (`playlistId`) REFERENCES `playlists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `playlist_followers_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `playlist_identity`
+--
+ALTER TABLE `playlist_identity`
+  ADD CONSTRAINT `playlist_identity_ibfk_1` FOREIGN KEY (`playlistId`) REFERENCES `playlists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `playlist_identity_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
